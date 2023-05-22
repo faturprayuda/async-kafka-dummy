985d60e15d2bdcf4a0a1538dbacc11dd9595e9fa
faturprayuda <muhammad.fathur.prayuda@bni.co.id>
2023-05-22 09:49:03 +0700

    init

---
+++ src/main/java/com/example/latihan/asynckafka/AsyncKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.latihan.asynckafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/latihan/asynckafka/AsyncKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihan.asynckafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
